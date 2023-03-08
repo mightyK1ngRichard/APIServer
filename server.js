d22ae91b7f0d9e8eef1e6bdccedb60ab4ea44d9b
Dmitiy <dimapermyakov55@gmail.com>
2023-03-08 19:01:55 +0300

    Add the pattern for sqlmanage

--- server.js
+++ server.js
@@ -1,36 +1,15 @@
-/* Старая реализация
-const http = require("http");
-const host = 'localhost';
-const port = 8000;
-
-const handler = (req, res) => {
-    /// req - http-запрос, res - http-ответ.
-    res.writeHead(200);
-    res.end('Курс ПСП такой крутой!');
-
-};
-
-const server = http.createServer(handler);
-
-/// ``` node server.js ``` - запускает сервер
-server.listen(port, host, () => {
-    console.log(`Сервер запущен по адресу http://${host}:${port}`);
-});
-*/
-
 const express = require('express');
 const fs = require("fs");
 const path = require("path");
 const app = express();
 const host = 'localhost';
 const port = 8000;
+const storageName = 'data/stocks.json';
 
 const readJson = (fileName) => {
     const file = fs.readFileSync(path.join(__dirname, fileName), "utf8");
     return JSON.parse(file);
 };
-
-const storageName = 'data/stocks.json';
 
 app.get('/stocks', (req, res) => {
     const stocks = readJson(storageName);
@@ -57,6 +36,12 @@ app.get('/stocks/:id', (req, res) => {
     }
 });
 
+app.get('/database/:command', (req, res) => {
+    const commendForSQL = req.params.command;
+    console.log('Тут бы я сделал запрос к sql: ', commendForSQL);
+    res.send(`Тут бы был результат SQL запроса: ${commendForSQL}`);
+})
+
 app.listen(port, host, () => {
-    console.log(`Сервер запущен по адресу http://${host}:${port}`);
+    console.log(`Сервер запущен по адресу  http://${host}:${port}`);
 });
